308fa1fcea170c5243194b999a7375e78bd82f51
pratyayj <pratyay.jaidev@gmail.com>
2020-10-30 01:45:01 +0800

    update traffic-count-cleaning

--- traffic/traffic-count-cleaning.scala
+++ traffic/traffic-count-cleaning.scala
@@ -1,5 +1,3 @@
-val df = spark.read.option("header", true).csv("/traffic-data/march-2019.csv")
-
 import org.apache.spark.sql.{SaveMode, SparkSession, Row}
 import org.apache.spark.sql.types._
 import org.apache.spark.sql.functions._
@@ -26,7 +24,7 @@ object TrafficCountCleaning {
     var df_wTotalSpeedCast = df_wTotalSpeed.withColumn("volume", col("volume").cast(DoubleType))
     
     // to get sum speed and volume for heavy and non-heavy vehicles
-    var grouped = df_wTotalSpeedCast.groupBy("read_date", "intersection_name", "direction")
+    var grouped = df_wTotalSpeedCast.groupBy("atd_device_id", "read_date", "intersection_name", "direction")
     var grouped_sum = grouped.sum("volume", "total_speed") // referenced as sum(volume) and sum(total_speed)
     grouped_sum = grouped_sum.withColumnRenamed("sum(volume)","sum_volume").withColumnRenamed("sum(total_speed)","sum_speed")
     
@@ -41,8 +39,7 @@ object TrafficCountCleaning {
     */
 
     //Write dataframe back to single csv file
-    val df_prepared = overall_average_speed
-      .coalesce(1)
+    val df_prepared = overall_average_speed.coalesce(1)
       .write
       .option("header", "true")
       .option("sep",",")
