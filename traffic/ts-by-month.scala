67da3183fe0f912e88e867086b4d556c3aa19d4f
Yogusvi Tewari <32561463+yogtew@users.noreply.github.com>
2020-11-28 14:48:16 +0800

    update input output paths and column name

--- traffic/ts-by-month.scala
+++ traffic/ts-by-month.scala
@@ -14,26 +14,26 @@ object TSByMonth {
     spark.sparkContext.setLogLevel("ERROR")
 
     // input and output file paths
-    val inputFiles = "/traffic-data/ts-by-intersection-day.csv/*.csv"
-    val outputFiles = "/traffic-data/ts-by-month.csv"
+    val inputFiles = "/traffic-data/ts-by-day/*.csv"
+    val outputFiles = "/traffic-data/ts-by-month"
 
     //spark read csv file
     val traffic_data = spark.read.option("header", true).csv(inputFiles)
 
     // cast column
-    var casted_data = traffic_data.withColumn("traffic_score", col("traffic_score").cast(DoubleType))
+    var casted_data = traffic_data.withColumn("weighted_TS", col("weighted_TS").cast(DoubleType))
 
     // create month and year columns
     val data = casted_data.withColumn("Month", split(col("day"),"-").getItem(1)).withColumn("Year", split(col("day"),"-").getItem(0))
     
     // group data by month and year and find overall TS
-    var grouped_data = data.groupBy("Month", "Year").avg("traffic_score")
+    var grouped_data = data.groupBy("Month", "Year").avg("weighted_TS")
 
     // clean data
-    grouped_data = grouped_data.orderBy("Year", "Month").withColumnRenamed("avg(traffic_score)","Traffic Score")
+    grouped_data = grouped_data.orderBy("Year", "Month").withColumnRenamed("avg(weighted_TS)","Traffic Score")
 
-    //Write dataframe back to single csv file
-    val intersectionCombinedByMonth = grouped_data.coalesce(1)
+    // Write dataframe back to single csv file
+    val TSCombinedByMonth = grouped_data.coalesce(1)
       .write
       .option("header", "true")
       .option("sep",",")
